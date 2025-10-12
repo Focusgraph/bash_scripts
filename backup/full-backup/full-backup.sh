23f7ef447d27fc387296edb08d1e2d0f7e3ee946
Focusgraph <ruben.ledesma.go@protonmail.com>
2025-10-12 23:45:20 +0200

    reorganized and removed outdated files

--- backup/full-backup/full-backup.sh
+++
@@ -1,5 +0,0 @@
-cd $(dirname $0)
-
-rsync --archive --acls --xattrs --delete --progress -h --exclude-from=exclude.txt / /mnt/backup
-
-rsync --archive --acls --xattrs --delete --progress -h --exclude-from=exclude.txt / /home/genty/Archive/Backups/agento_backup
--- backup/sync-backup.sh
+++
@@ -1,1 +0,0 @@
-rsync --archive --acls --xattrs --delete --progress -h /home/genty/Sync/ /home/genty/Archive/Backups/Sync
